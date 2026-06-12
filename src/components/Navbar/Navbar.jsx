78f24650a3dab16d88a2a2ad3671ba80364783eb
ayushipathak2025 <ayushi.pathak2025@vitstudent.ac.in>
2026-06-12 12:35:52 +0530

    Enlarge SSV logo to fill navbar whitespace

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         {/* Logo */}
         <a href="#" className="navbar__logo" id="logo">
           <div className="navbar__logo-icon">
-            <img src={`${import.meta.env.BASE_URL}logo-star.png`} alt="SSV Logo" style={{ width: '120px', height: '120px', objectFit: 'contain' }} />
+            <img src={`${import.meta.env.BASE_URL}logo-star.png`} alt="SSV Logo" style={{ height: '160px', width: 'auto', objectFit: 'contain', display: 'block' }} />
           </div>
         </a>
 
