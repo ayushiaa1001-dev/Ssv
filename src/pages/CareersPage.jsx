fc0f54d89c615ea02ca46f579975db82028f9622
ayushipathak2025 <ayushi.pathak2025@vitstudent.ac.in>
2026-06-14 22:00:46 +0530

    clean: remove unused useRef import in CareersPage.jsx

--- src/pages/CareersPage.jsx
+++ src/pages/CareersPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { useIntersectionObserver } from '../hooks/useIntersectionObserver'
 import './CareersPage.css'
