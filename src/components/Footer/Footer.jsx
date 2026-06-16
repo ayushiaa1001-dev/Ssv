3d9d52fe761b45a069a9d51b9feb6f8d15778e86
ayushipathak2025 <ayushi.pathak2025@vitstudent.ac.in>
2026-06-17 00:25:50 +0530

    fix: Audit #6 - disable hover underline on non-clickable footer address

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -67,7 +67,7 @@ const Footer = () => {
           <div className="footer__col">
             <h4 className="footer__col-title">Contact Us</h4>
             <div className="footer__contact-list">
-              <span className="footer__contact-item">
+              <span className="footer__contact-item footer__contact-item--static">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                   <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z"/>
                   <circle cx="12" cy="10" r="3"/>
