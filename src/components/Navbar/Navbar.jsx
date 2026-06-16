bd1dc9f539c2fdca2a4ab0ba7c1e5d277f3c99cb
ayushipathak2025 <ayushi.pathak2025@vitstudent.ac.in>
2026-06-16 16:38:49 +0530

    style: Increase pentagon logo size to 95px

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -359,7 +359,7 @@ const Navbar = () => {
 
           <button className="btn navbar__cta" id="contact-btn" onClick={handleContactClick}>Contact Us</button>
           <div className="navbar__badge" title="SSV Pharmaceuticals Quality Seal">
-            <img src={`${import.meta.env.BASE_URL}logo-pentagon.png`} alt="SSV Quality Seal" style={{ width: '80px', height: '80px', objectFit: 'contain' }} />
+            <img src={`${import.meta.env.BASE_URL}logo-pentagon.png`} alt="SSV Quality Seal" style={{ width: '95px', height: '95px', objectFit: 'contain' }} />
           </div>
         </div>
 
