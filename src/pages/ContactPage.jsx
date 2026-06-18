2e3bec985ba9f41df0eab050cee16af1a4ae17d0
ayushipathak2025 <ayushi.pathak2025@vitstudent.ac.in>
2026-06-18 17:36:31 +0530

    contact us hero banner change

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -54,7 +54,7 @@ const ContactPage = () => {
       <section className="ct-hero">
         <div className="ct-hero__bg">
           <img
-            src="https://images.unsplash.com/photo-1521791136064-7986c2920216?w=1600&q=80"
+            src="https://images.unsplash.com/photo-1596524430615-b46475ddff6e?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Contact SSV Pharmaceuticals"
             loading="lazy"
           />
